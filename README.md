178bdde3d7839cced9411e29fcd692c71f186b0b
Ronald Huidrom <ronhuidrom@gmail.com>
2022-07-03 16:19:06 +0500

    Update source files and add README.md

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# good-old-sea
-Some good files for the good old C (sort of a guide)
+<p align="center">
+  <img src="images/logo.png" />
+</p>
+
+<h1 align="center">
+  Good Old Sea
+</h1>
+
+<p align="center">
+  <img src="https://img.shields.io/badge/C-00599C?style=for-the-badge&logo=c&logoColor=white" />
+  <img src="https://img.shields.io/badge/GNU%20Bash-4EAA25?style=for-the-badge&logo=GNU%20Bash&logoColor=white" />
+  <img src="https://img.shields.io/badge/Emacs-%237F5AB6.svg?&style=for-the-badge&logo=gnu-emacs&logoColor=white" />
+</p>
+  
+After so many years and decades, the world is still powered by the C programming language. Here are some source codes written entirely in C.
+
+All the source files have been tested to compile and execute. All of them are written on Emacs. They are compiled and executed on bash. See the list of all source files below in `contents` section.
+
+### Compilation
+
+On bash, the source [io.c](io.c) can be compiled using `gcc` as follows:
+
+```bash
+gcc -o io io.c
+```
+
+A new executable file with the name `io` will be created in the same directory. Do the same for other source files.
+
+### Execution
+
+The executable file `io` which was created in the compilation step can now be run (executed) as follows:
+
+```bash
+./io
+```
+
+Do the same for other executables.
+
+### Errors
+
+Ideally, the compilation shouldn't generate any errors. However, compiling some source files such as [containers.c](containers.c) might generate an error due to `math.h` header file. In this case, compile as follows:
+
+```bash
+gcc -o containers containers.c -lm
+```
+
+Do the same for any source file that includes `math.h` as a header file.
+
+### Commentation
+
+The source files are commented well. This should help any new learner of C and anyone else in general.
+
+### Contents
+
+| Source                        | Topics          |
+| :---                          | :---            |
+| [container.c](container.c)    | C Containers: Arrays, Structures, Unions, Pointers |
+| [methods.c](methods.c)        | Functions: Declaration, Definition, By Value, By Pointer, Recursion, Function Pointers |
+| [io.c](io.c)                  | Standard I/O, File I/O, Read and Write Strings |
+
+### Future
+
+More source files shall be added soon.
--- io.c
+++ io.c
@@ -1,5 +1,5 @@
 /* IO : Standard input/output
-        Files,
+        File input/ouput
         Strings */
 
 #include <stdio.h>
@@ -11,7 +11,9 @@ int main() {
   /// Standard output
   /////////////////////////////////////////////////////////////////////////  
 
-  char          ch = 's';
+  printf("STANDARD OUTPUT\n");
+  
+  char          cc = 's';
   int           in = 24;
   double        db = 7345.7639;
   int          *pt = (int *) malloc(sizeof(int));
@@ -21,7 +23,7 @@ int main() {
   unsigned long ul = 264;
 
   // output
-  printf("%c\n", ch);                    // %c  : character
+  printf("%c\n", cc);                    // %c  : character
   printf("%d\n", in);                    // %d  : integer
   printf("%.2f\n", db);                  // %f  : float / double
   printf("%e\n", db);                    // %e  : scientic notation
@@ -34,26 +36,167 @@ int main() {
   printf("%lu\n", ul);                   // %lu : unsigned int, unsigned long
 
   // multiple output
-  printf("%d %c\n", in, ch);             // note the space in between %d and %c
+  printf("%d %c\n", in, cc);             // note the space in between %d and %c
   
   // input
-  scanf("%d %c", &in, &ch);              // input integer, char
+  printf("Waiting for an input ... enter [int] [char]\n");
+  scanf("%d %c", &in, &cc);              // input integer, char
   // always input together in one line
 
   // output the new values
-  printf("%d %c\n", in, ch);
+  printf("%d %c\n", in, cc);
 
   char *name = (char *) malloc(sizeof(char) * 10);
   
   // input
-  scanf("%s", name);                       // note that there is no &
+  printf("Waiting for an input ... enter a string\n");
+  scanf("%s", name);                              // note that there is no &
 
   // output the new string value
   printf("%s\n", name);
+  printf("\n");
+  
+  //////////////////////////////////////////////////////////////////////
+  /// File input
+  //////////////////////////////////////////////////////////////////////
 
+  printf("FILE INPUT\n");
+
+  FILE *fp = NULL;                                // pointer to handle io using buffers
+
+  unsigned inum;
+
+  fp = fopen("number", "r");                      // open in read mode
+  if (!fp) {
+    fprintf(stderr, "Failed to open file.\n");    // standard error msg
+    exit(1);                                      // provided by stdlib.h
+  }
+  // fscanf() reads a set of data from a file
+  fscanf(fp, "%d", &inum);
+  printf("%d\n", inum);
+  fclose(fp);                                     // clear buffers and close file
+
+  fp = fopen("text", "r");
+  char ch;                                        // to get each character from the file one by one
+
+  if (!fp) {
+    fprintf(stderr, "Failed to open file.\n");
+    exit(1);
+  }
+
+  // getc() gets individual character from file
+  do {
+    ch = getc(fp);
+    printf("%c", ch);
+  } while (ch != '\n');                           // until newline
+  fclose(fp);
+  
+  // getw() gets integer from file
+
+  printf("\n");
+  
   //////////////////////////////////////////////////////////////////////
-  /// Files
+  /// File output
   //////////////////////////////////////////////////////////////////////
+
+  printf("FILE OUTPUT\n");
+  
+  fp = fopen("plain.txt", "w");                      // write mode
+  if (!fp) {
+    fprintf(stderr, "Failed to open file.\n");
+    exit(1);
+  }
+
+  char *waifu = "Kurisu Makise";
+  unsigned c = 0;
+
+  do {
+    // putc writes individual character to a file
+    // if the file extension of the file is not specified, the output is a binary file
+    putc(*(waifu+c), fp);                           // note the argument list
+    if (*(waifu+c) == '\0') {                       // end of string
+      putc('\n', fp);
+      break;
+    }
+    c++;
+  } while (1);
+  fclose(fp);
+
+  fp = fopen("plain.txt", "r");
+  while ( (ch = getc(fp)) != EOF) {
+    printf("%c", ch);
+  }
+
+  unsigned age = 22;
+  
+  // fprintf() writes a set of data to file
+  fp = fopen("record", "w");
+  printf("Writing to file now ...\n");
+  fprintf(fp, "Name : %s\t Age : %u\n", waifu, age);  // note the argument list
+  printf("Wrote to file.\n");
+  fclose(fp);
+
+  // fscanf() to retrieve a set of data from file
+  char sentence[50];
+  fp = fopen("record", "r");
+  printf("Checking what is written ...\n");
+  c = 0;                                              // loop counter
+  while (c < 7) {
+    fscanf(fp, "%s", sentence);
+    if (*sentence == EOF) {
+      printf("\n");
+      break;
+    }
+    printf("%s ", sentence);
+    c++;
+  }
+
+  printf("\n\n");
+
+  // file i/o functions: getc(), getw(), putc(), fprintf(), fscanf()
+
+  // getchar(), putchar()
+  
+  //////////////////////////////////////////////////////////////////////
+  /// Read and write strings
+  //////////////////////////////////////////////////////////////////////
+
+  printf("READ AND WRITE STRINGS\n");
+
+  char *str = "C programming";                         // str[] = "C programming";
+  printf("%s\n", str);                                 // print string
+
+  str = "Kisuke Urahara";                              // string changed
+  printf("%s\n", str);                                 // output the change
+
+  char *substr_1 = (char *) malloc(sizeof(char) * 10);
+  char *substr_2 = (char *) malloc(sizeof(char) * 10);
+
+  // read and tokenize
+  sscanf(str, "%s %s", substr_1, substr_2);            // tokenize by space
+  printf("Input string to be parsed : %s\n", str);     // Kisuke Urahara
+  printf("Token 1 : %s\n", substr_1);                  // Kisuke
+  printf("Token 2 : %s\n", substr_2);                  // Urahara
+
+  free(substr_1);
+  free(substr_2);
+
+  char *date = "25 December 1996";
+  printf("%s\n", str);
+
+  int day = 0, year = 0;
+  char *month = (char *) malloc(sizeof(char) * 10);
+
+  // read and tokenize
+  sscanf(date, "%d %s %d", &day, month, &year);         // note the use of &
+  printf("Date to be parsed : %s\n", date);
+  printf("Day : %d\n", day);
+  printf("Month : %s\n", month);
+  printf("Year : %d\n", year);
+
+  free(month);
+
+  printf("\n");
   
   return 0;
 }
